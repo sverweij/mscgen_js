0a449c1c7a8bcadd384b0a2e2c7dc8df70d3fc60
Sander Verweij <foureightone@gmail.com>
2013-07-15 00:00:12 +0200

    Update msc2msc.js

--- script/node/msc2msc.js
+++ script/node/msc2msc.js
@@ -1,3 +1,8 @@
+/* 
+ * reads a mscgen program from input, parses it and (standardly
+ * formatted) outputs it again
+ */
+
 var parser = require ("./mscgenparser_node");
 var pt2mscgen = require ("./parsetree2mscgen");
 
@@ -16,10 +21,19 @@ process.stdin.on('end', function() {
     process.stdin.pause();
 });
 
-
 /*
-fs.readFile('input.msc', function (err, data) {
-    if (err) throw err;
-        console.log(data);
-});
+    This file is part of mscgen_js.
+
+    mscgen_js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    mscgen_js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with mscgen_js.  If not, see <http://www.gnu.org/licenses/>.
 */
