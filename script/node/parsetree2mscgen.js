c533791a9e10f59f867ff61f92714ef657a16c12
Sander Verweij <foureightone@gmail.com>
2013-07-15 00:02:09 +0200

    Update parsetree2mscgen.js

--- script/node/parsetree2mscgen.js
+++ script/node/parsetree2mscgen.js
@@ -1,3 +1,7 @@
+/*
+ * takes a parsetree for a message sequence chart and renders it
+ * as an mscgen program. 
+ */
 module.exports = (function(){
 const INDENT = "  ";
 
@@ -138,3 +142,19 @@ var result =  {
 
 return result;
 })();
+/*
+    This file is part of mscgen_js.
+
+    mscgen_js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    mscgen_js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with mscgen_js.  If not, see <http://www.gnu.org/licenses/>.
+*/
