50b3f309313daa86c635a672460b28bc873cc8be
sverweij <481@nepserver.fake>
2013-08-11 12:07:09 +0200

    set up rest of build automation (issue #6)

--- build.sh
+++ build.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 # usage: build all pages & css: build.sh
-#        only build workout and associated files: build.sh workout
+#        only build scripts and associated files: build.sh scripts
 #        build all css: build.sh css
 
+# TODO: test for existence of pegjs (and npm it if not there?)
+
+echo "building mscgen parser from peg (web)  ..."
+pegjs --export-var var\ mscparser script/node/mscgenparser.pegjs script/mscgenparser.js
+echo "building mscgen parser from peg (node)  ..."
+pegjs script/node/mscgenparser.pegjs script/node/mscgenparser_node.js
+
+echo "building msgenny parser from peg (web) ..."
+pegjs --export-var var\ msgennyparser script/node/msgennyparser.pegjs script/msgennyparser.js
+echo "building msgenny parser from peg (node) ..."
+pegjs script/node/msgennyparser.pegjs script/node/msgennyparser_node.js
+
+# TODO: test for existence of sed (and what to do if it isn't there?)
+echo "creating web variants of parsetree2*.js ..."
+sed s/module.exports/var\ tomscgen/g script/node/parsetree2mscgen.js > script/parsetree2mscgen.js
+sed s/module.exports/var\ tomsgenny/g script/node/parsetree2msgenny.js > script/parsetree2msgenny.js
+
+echo "optimizing css and javascript with r.js ..."
+# TODO: test for existence of r.js (and npm it if not there/ or use
+# cp yadda-src.js yadda.js which also works but isn't as efficient
+# on page loading)
 for i in build.*$1*.js; do
     r.js -o $i
 done
