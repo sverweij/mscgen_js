78eecd18e3e364517501be9cac5a4efabf524564
sverweij <481@nepserver.fake>
2013-08-17 10:12:37 +0200

    remove build.attempt, adapt deploy.sh for split src/ deployable

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # TODO: error flows
 git checkout gh-pages
-git merge master
+git merge master -m "merge for gh-pages build"
+sh build.sh
+git commit -a -m "build"
 git push
 git checkout master
