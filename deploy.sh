501f04faa97779dd45428ca343a6234da6344e55
sverweij <481@nepserver.fake>
2013-09-28 11:57:40 +0200

    build

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,6 @@ git checkout gh-pages
 git merge master -m "merge for gh-pages build"
 sh build.sh
 git add .
-<<<<<<< HEAD
 git commit -m "build"
-=======
-git commit -a -m "build"
->>>>>>> master
 git push
 git checkout master
