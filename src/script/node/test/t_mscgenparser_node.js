912638a1e5f34d4cfe34fb70ed385d627de5f6f1
sverweij <481@nepserver.fake>
2013-10-21 20:54:29 +0200

    add unit tests for mscgen parser

---
+++ src/script/node/test/t_mscgenparser_node.js
@@ -0,0 +1,44 @@
+var assert = require("assert");
+var parser = require("../mscgenparser_node");
+
+describe('mscgenparser', function() {
+    describe('#parse() - simple script', function() {
+
+        it('should render a simple AST, with two entities auto declared', function() {
+            lAST = parser.parse('msc { a,b; a => b [label="a simple mscgen script"];}');
+            assert.equal(JSON.stringify(lAST), JSON.stringify({
+                "entities" : [{
+                    "name" : "a"
+                }, {
+                    "name" : "b"
+                }],
+                "arcs" : [[{
+                    "kind" : "=>",
+                    "from" : "a",
+                    "to" : "b",
+                    "label" : "a simple mscgen script"
+                }]]
+            }));
+        });
+
+        it("should produce an (almost empty) AST for empty input");
+        it("should produce an AST even when non entity arcs are its only content");
+        it("should produce lowercase for upper/ mixed case arc kinds");
+        it("should produce lowercase for upper/ mixed case options");
+        it("should produce only 'true' or 'false' for all variants of wordwraparcs");
+        it("should throw a SyntaxError on an invalid program", function() {
+            try {
+                var lAST = parser.parse('a');
+                var lStillRan = false;
+                if (lAST) {
+                    lStillRan = true;
+                }
+                assert.equal(lStillRan, false);
+            } catch(e) {
+                assert.equal(e.name, "SyntaxError");
+            }
+
+        });
+    });
+});
+
