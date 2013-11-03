9071fa7e5552df90dfe5252bf4305f87d9a4aa55
sverweij <481@nepserver.fake>
2013-11-03 15:12:36 +0100

    Add a favicon

---
+++ utl/png2favico.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+optipng -quiet $1
+convert $1  -bordercolor white -border 0 \
+          \( -clone 0 -resize 16x16 \) \
+          \( -clone 0 -resize 24x24 \) \
+          \( -clone 0 -resize 32x32 \) \
+          \( -clone 0 -resize 48x48 \) \
+          \( -clone 0 -resize 64x64 \) \
+          -delete 0 -alpha off -colors 256 $2
