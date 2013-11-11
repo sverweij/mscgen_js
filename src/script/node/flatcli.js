d4c3a15b850135bc837c5b35ed87b7fc35f78289
sverweij <481@nepserver.fake>
2013-11-11 21:33:15 +0100

    update flatcli excerciser with broadcast

--- src/script/node/flatcli.js
+++ src/script/node/flatcli.js
@@ -12,8 +12,8 @@ var lAST = parser.parse ('msc { \
     c note c [label="automatically colorize to yellow/ black"]; \
     b note b [label="do not color as already has color", linecolor="red"];\
 }');
-var lAST = parser.parse('msc{ a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t; b note c; d box e; a rbox a; e abox d;}');
-// lAST = x.flatten(lAST);
+var lAST = parser.parse('msc{ arcgradient=20; a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t; b note c; d box e; a rbox a; e abox d; e =>> *;}');
+lAST = x.flatten(lAST);
 lAST = c.colorize(lAST);
 
 
