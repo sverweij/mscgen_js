5060cedabf52eb8ad97a1638be7ba7222d2ac4da
sverweij <481@nepserver.fake>
2013-11-13 20:27:39 +0100

    remove endsWith shim - not necessary anymore

--- src/script/node/textutensils.js
+++ src/script/node/textutensils.js
@@ -123,23 +123,6 @@ define([], function() {
         return "mscgen";
 
     }
-/*
-    function _setupStringShims() {
-        if (!String.prototype.endsWith) {
-            Object.defineProperty(String.prototype, 'endsWith', {
-                enumerable : false,
-                configurable : false,
-                writable : false,
-                value : function(searchString, position) {
-                    position = position || this.length;
-                    position = position - searchString.length;
-                    var lastIndex = this.lastIndexOf(searchString);
-                    return lastIndex !== -1 && lastIndex === position;
-                }
-            });
-        }
-    }
-    */
 
     return {
         wrap : function(pText, pMaxLength) {
@@ -157,10 +140,6 @@ define([], function() {
         classify : function(pString) {
             return _classify(pString);
         }
-        /*, 
-        setupStringShims : function() {
-            return _setupStringShims();
-        }*/
     };
 });
 /*
