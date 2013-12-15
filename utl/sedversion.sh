94b92c19f0f6d91f9d7ea91e1b94b38a7d32cbf1
sverweij <481@nepserver.fake>
2013-12-15 12:22:09 +0100

    generate smaller commit ids in the version toting sources

--- utl/sedversion.sh
+++ utl/sedversion.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 VERSION=`cat VERSION`
-COMMIT=`git rev-parse HEAD`
+COMMIT=`git rev-parse HEAD | cut -c 1-16`
 DATE=`date "+%Y%m%d %H:%M"`
 sed  -E s/"([^\$])([\$]{version})"/"\1$VERSION"/g |\
     sed  -E s/"([^\$])([\$]{commit})"/"\1$COMMIT"/g |\
