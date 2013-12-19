005c84437747174e6c9b369fd2fc366dfdd42f55
sverweij <481@nepserver.fake>
2013-12-19 08:39:34 +0100

    switched found & expected so mocha output is now correct

--- src/script/node/test/testutensils.js
+++ src/script/node/test/testutensils.js
@@ -1,13 +1,13 @@
 var assert = require("assert");
 
 module.exports = (function() {
-    function _assertequalJSON(pLeft, pRight) {
-        assert.equal(JSON.stringify(pLeft), JSON.stringify(pRight));
+    function _assertequalJSON(pExpected, pFound) {
+        assert.equal(JSON.stringify(pFound), JSON.stringify(pExpected));
     }
 
     return {
-        assertequalJSON : function(pLeft, pRight) {
-            return _assertequalJSON(pLeft, pRight);
+        assertequalJSON : function(pExpected, pFound) {
+            return _assertequalJSON(pExpected, pFound);
         }
     };
 })();
