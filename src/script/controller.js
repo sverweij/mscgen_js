752d5e4b194ddec4e8d6d51bec9735192218a0db
sverweij <481@nepserver.fake>
2014-05-03 11:41:45 +0200

    Add "to embedded in html"  button to demo

--- src/script/controller.js
+++ src/script/controller.js
@@ -184,6 +184,12 @@ function setupEvents () {
                     gaga.g('send', 'event', 'show_jpeg_base64', 'button');
                 }
     });
+    $("#__show_html").bind({
+        click : function(e) {
+                    show_htmlOnClick();
+                    gaga.g('send', 'event', 'show_html', 'button');
+                }
+    });
     $("#__show_dot").bind({
         click : function(e) {
                     show_dotOnClick();
@@ -326,6 +332,8 @@ function getAST(pInput, pLanguage) {
         lAST = msgennyparser.parse(pInput);
     } else if ("json" === pLanguage) {
         lAST = JSON.parse(pInput);
+    } else if ("xu" === pLanguage) {
+        lAST = mscparser.parse(pInput);
     } else {
         lAST = mscparser.parse(pInput);
     }
@@ -342,13 +350,12 @@ function switchLanguageOnClick (pValue) {
         if (lAST !== {}){
             if ("msgenny" === pValue){
                 gCodeMirror.setValue(tomsgenny.render(lAST));
-                // gCodeMirror.setOption("mode", "msgenny");
             } else if ("json" === pValue){
                 gCodeMirror.setValue(JSON.stringify(lAST, null, "  "));
-                // gCodeMirror.setOption("mode", "json");
+            } else if ("xu" === pValue){
+                gCodeMirror.setValue(tomscgen.render(lAST));
             } else {
                 gCodeMirror.setValue(tomscgen.render(lAST));
-                // gCodeMirror.setOption("mode", "mscgen");
             }
         }
     } catch(e) {
@@ -363,8 +370,8 @@ function clearOnClick(){
     if ("msgenny" === gLanguage){
         gCodeMirror.setValue("");
     } else if ("json" === gLanguage){
-        gCodeMirror.setValue("");        
-    } else {
+        gCodeMirror.setValue("");   
+    } else /* "mscgen" === gLanguage || "xu" === gLanguage */{
         gCodeMirror.setValue("msc{\n  \n}");
         gCodeMirror.setCursor(1,3);
     }
@@ -405,6 +412,8 @@ function reRenderSource(pAST){
         gCodeMirror.setValue(tomsgenny.render(pAST));
     } else if ("json" === gLanguage){
         gCodeMirror.setValue(JSON.stringify(pAST, null, "  "));
+    } else if ("xu" === gLanguage){
+        gCodeMirror.setValue(tomscgen.render(pAST));
     } else {
         gCodeMirror.setValue(tomscgen.render(pAST));
     }
@@ -483,6 +492,10 @@ function show_rasterOnClick (pType) {
     var lWindow = window.open(toRasterURI("#__svg", pType), "_blank");
 }
 
+function show_htmlOnClick(){
+    var lWindow = window.open('data:text/plain;charset=utf-8,'+encodeURIComponent("<!DOCTYPE html>\n<html>\n<head>\n<script data-main='https://sverweij.github.io/mscgen_js/script/mscgen-inpage.js' src='https://sverweij.github.io/mscgen_js/lib/require.js'></script>\n<head>\n<body>\n<pre class='code " + gLanguage + " mscgen_js' data-language='" + gLanguage +"'>\n" + gCodeMirror.getValue() + "\n</pre>\n</body>\n</html>"));
+}
+
 function show_dotOnClick(){
     var lWindow = window.open('data:text/plain;charset=utf-8,'+encodeURIComponent(todot.render(getAST(gCodeMirror.getValue(), gLanguage))));
 }
@@ -503,7 +516,6 @@ function show_communications_diagramOnClick(){
 }
 
 function show_urlOnClick(){
-    // window.location = 
     var lWindow = window.open('data:text/plain;charset=utf-8,'+
         encodeURIComponent(
             window.location.protocol + '//' +
@@ -548,7 +560,7 @@ function showLanguageState () {
         $("#__btn_colorize_hard").show();
         $("#__btn_weigh").show();
         $("#__btn_ioweigh").show();
-    } else {
+    } else /* "mscgen" === gLanguage || "xu" === gLanguage */{
         $("#__language_mscgen").prop("checked", "msgennyOn");
         $("#__language_msgenny").removeAttr("checked", "msgennyOn");
         $("#__language_json").removeAttr("checked", "msgennyOn");
