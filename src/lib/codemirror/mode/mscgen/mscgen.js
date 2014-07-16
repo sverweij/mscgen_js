f20d0b7974a9c27e26ad377984b6bb2b34acae31
sverweij <481@nepserver.fake>
2014-07-16 22:47:17 +0200

    unshabby codemirror highlighter mode (issue #31)

--- src/lib/codemirror/mode/mscgen/mscgen.js
+++ src/lib/codemirror/mode/mscgen/mscgen.js
@@ -1,166 +1,192 @@
 (function(mod) {
-    if ( typeof exports == "object" && typeof module == "object")// CommonJS
-        mod(require("../../lib/codemirror"));
-    else if ( typeof define == "function" && define.amd)// AMD
-        define(["../../lib/codemirror"], mod);
-    else// Plain browser env
-        mod(CodeMirror);
+	if ( typeof exports == "object" && typeof module == "object")// CommonJS
+		mod(require("../../lib/codemirror"));
+	else if ( typeof define == "function" && define.amd)// AMD
+		define(["../../lib/codemirror"], mod);
+	else// Plain browser env
+		mod(CodeMirror);
 })(function(CodeMirror) {//
-    "use strict";
+	"use strict";
 
-    CodeMirror.defineMode("mscgen", function(config, parserConfig) {
-        return {
-            startState : produceStartStateFunction(),
-            token : produceTokenFunction({
-                "keywords" : ["msc"],
-                "options" : ["hscale", "width", "arcgradient", "wordwraparcs"],
-                "attributes" : ["label", "idurl", "id", "url", "linecolor", "linecolour", "textcolor", "textcolour", "textbgcolor", "textbgcolour", "arclinecolor", "arclinecolour", "arctextcolor", "arctextcolour", "arctextbgcolor", "arctextbgcolour", "arcskip"],
-                "brackets" : [/*"\\[", "\\]", */"\\{", "\\}"],
-                "arcs" : ["\\|\\|\\|", "\\.\\.\\.", "---", "--", "<->", "==", "<<=>>", "<=>", "\\.\\.", "<<>>", "::", "<:>", "->", "=>>", "=>", ">>", ":>", "-x", "<-", "<<=", "<=", "<<", "<:", "-x", "note", "abox", "rbox", "box"],
-                "singlecomment" : ["//", "#"],
-                "operators" : ["="]
-            }),
-            lineComment : "#",
-            blockCommentStart : "/*",
-            blockCommentEnd : "*/"
-        };
-    });
-    CodeMirror.defineMIME("text/mscgen", "mscgen");
+	CodeMirror.defineMode("mscgen", function(config, parserConfig) {
+		return {
+			startState : produceStartStateFunction(),
+			copyState : produceCopyStateFunction(),
+			token : produceTokenFunction({
+				"keywords" : ["msc"],
+				"options" : ["hscale", "width", "arcgradient", "wordwraparcs"],
+				"attributes" : ["label", "idurl", "id", "url", "linecolor", "linecolour", "textcolor", "textcolour", "textbgcolor", "textbgcolour", "arclinecolor", "arclinecolour", "arctextcolor", "arctextcolour", "arctextbgcolor", "arctextbgcolour", "arcskip"],
+				"brackets" : [/*"\\[", "\\]", */"\\{", "\\}"],
+				"arcsWords" : ["note", "abox", "rbox", "box"],
+				"arcsOthers" : ["\\|\\|\\|", "\\.\\.\\.", "---", "--", "<->", "==", "<<=>>", "<=>", "\\.\\.", "<<>>", "::", "<:>", "->", "=>>", "=>", ">>", ":>", "<-", "<<=", "<=", "<<", "<:", "x-", "-x"],
+				"singlecomment" : ["//", "#"],
+				"operators" : ["="]
+			}),
+			lineComment : "#",
+			blockCommentStart : "/*",
+			blockCommentEnd : "*/"
+		};
+	});
+	CodeMirror.defineMIME("text/mscgen", "mscgen");
 
-    CodeMirror.defineMode("xu", function(config, parserConfig) {
-        return {
-            startState : produceStartStateFunction(),
-            token : produceTokenFunction({
-                "keywords" : ["msc"],
-                "options" : ["hscale", "width", "arcgradient", "wordwraparcs", "watermark"],
-                "attributes" : ["label", "idurl", "id", "url", "linecolor", "linecolour", "textcolor", "textcolour", "textbgcolor", "textbgcolour", "arclinecolor", "arclinecolour", "arctextcolor", "arctextcolour", "arctextbgcolor", "arctextbgcolour", "arcskip"],
-                "brackets" : [/*"\\[", "\\]", */"\\{", "\\}"],
-                "arcs" : ["\\|\\|\\|", "\\.\\.\\.", "---", "--", "<->", "==", "<<=>>", "<=>", "\\.\\.", "<<>>", "::", "<:>", "->", "=>>", "=>", ">>", ":>", "-x", "<-", "<<=", "<=", "<<", "<:", "-x", "note", "abox", "rbox", "box", "alt", "else", "opt", "break", "par", "seq", "strict", "neg", "critical", "ignore", "consider", "assert", "loop", "ref", "exc"],
-                "singlecomment" : ["//", "#"],
-                "operators" : ["="]
-            }),
-            lineComment : "#",
-            blockCommentStart : "/*",
-            blockCommentEnd : "*/"
-        };
-    });
-    CodeMirror.defineMIME("text/xu", "xu");
+	CodeMirror.defineMode("xu", function(config, parserConfig) {
+		return {
+			startState : produceStartStateFunction(),
+			copyState : produceCopyStateFunction(),
+			token : produceTokenFunction({
+				"keywords" : ["msc"],
+				"options" : ["hscale", "width", "arcgradient", "wordwraparcs", "watermark"],
+				"attributes" : ["label", "idurl", "id", "url", "linecolor", "linecolour", "textcolor", "textcolour", "textbgcolor", "textbgcolour", "arclinecolor", "arclinecolour", "arctextcolor", "arctextcolour", "arctextbgcolor", "arctextbgcolour", "arcskip"],
+				"brackets" : [/*"\\[", "\\]", */"\\{", "\\}"],
+				"arcsWords" : ["note", "abox", "rbox", "box", "alt", "else", "opt", "break", "par", "seq", "strict", "neg", "critical", "ignore", "consider", "assert", "loop", "ref", "exc"],
+				"arcsOthers" : ["\\|\\|\\|", "\\.\\.\\.", "---", "--", "<->", "==", "<<=>>", "<=>", "\\.\\.", "<<>>", "::", "<:>", "->", "=>>", "=>", ">>", ":>", "<-", "<<=", "<=", "<<", "<:", "x-", "-x"],
+				"singlecomment" : ["//", "#"],
+				"operators" : ["="]
+			}),
+			lineComment : "#",
+			blockCommentStart : "/*",
+			blockCommentEnd : "*/"
+		};
+	});
+	CodeMirror.defineMIME("text/xu", "xu");
 
-    CodeMirror.defineMode("msgenny", function(config, parserConfig) {
-        return {
-            startState : produceStartStateFunction(),
-            token : produceTokenFunction({
-                "keywords" : null,
-                "options" : ["hscale", "width", "arcgradient", "wordwraparcs", "watermark"],
-                "attributes" : null,
-                "brackets" : ["\\{", "\\}"],
-                "arcs" : ["\\|\\|\\|", "\\.\\.\\.", "---", "--", "<->", "==", "<<=>>", "<=>", "\\.\\.", "<<>>", "::", "<:>", "->", "=>>", "=>", ">>", ":>", "-x", "<-", "<<=", "<=", "<<", "<:", "-x", "note", "abox", "rbox", "box", "alt", "else", "opt", "break", "par", "seq", "strict", "neg", "critical", "ignore", "consider", "assert", "loop", "ref", "exc"],
-                "singlecomment" : ["//", "#"],
-                "operators" : ["="]
-            }),
-            lineComment : "#",
-            blockCommentStart : "/*",
-            blockCommentEnd : "*/"
+	CodeMirror.defineMode("msgenny", function(config, parserConfig) {
+		return {
+			startState : produceStartStateFunction(),
+			copyState : produceCopyStateFunction(),
+			token : produceTokenFunction({
+				"keywords" : null,
+				"options" : ["hscale", "width", "arcgradient", "wordwraparcs", "watermark"],
+				"attributes" : null,
+				"brackets" : ["\\{", "\\}"],
+				"arcsWords" : ["note", "abox", "rbox", "box", "alt", "else", "opt", "break", "par", "seq", "strict", "neg", "critical", "ignore", "consider", "assert", "loop", "ref", "exc"],
+				"arcsOthers" : ["\\|\\|\\|", "\\.\\.\\.", "---", "--", "<->", "==", "<<=>>", "<=>", "\\.\\.", "<<>>", "::", "<:>", "->", "=>>", "=>", ">>", ":>", "<-", "<<=", "<=", "<<", "<:", "x-", "-x"],
+				"singlecomment" : ["//", "#"],
+				"operators" : ["="]
+			}),
+			lineComment : "#",
+			blockCommentStart : "/*",
+			blockCommentEnd : "*/"
 
-        };
-    });
-    CodeMirror.defineMIME("text/msgenny", "msgenny");
+		};
+	});
+	CodeMirror.defineMIME("text/msgenny", "msgenny");
 
-    function wordRegexp(words) {
-        return new RegExp("((" + words.join(")|(") + "))", "i");
-    }
+	function wordRegexpBoundary(words) {
+		return new RegExp("[^a-z0-9]((" + words.join(")|(") + "))\\b", "i");
+	}
 
-    function produceStartStateFunction() {
-        return function() {
-            return {
-                inComment : false,
-                inString : false,
-                inAttributeList : false,
-                inScript : false
-            };
-        };
-    }
+	function wordRegexp(words) {
+		return new RegExp("((" + words.join(")|(") + "))", "i");
+	}
 
-    function produceTokenFunction(pConfig) {
+	function produceStartStateFunction() {
+		return function() {
+			return {
+				inComment : false,
+				inString : false,
+				inAttributeList : false,
+				inScript : false
+			};
+		};
+	}
 
-        return function(stream, state) {
-            // if (!pConfig.inScript) {
-            // if (pConfig.keywords !== null && stream.match(wordRegexp(pConfig.keywords), true, true)) {
-            // return "keyword";
-            // }
-            // if (stream.match(/{/, true, true)) {
-            // pConfig.inScript = true;
-            // return "bracket";
-            // }
-            // }
-            // if (pConfig.inScript) {
-            if (stream.match(/}/, true, true)) {
-                pConfig.inScript = false;
-                return "bracket";
-            }
-            if (stream.match(wordRegexp(pConfig.brackets), true, true)) {
-                return "bracket";
-            }
-            if (stream.match(wordRegexp(pConfig.options), true, true)) {
-                return "keyword";
-            }
-            if (stream.match(wordRegexp(pConfig.arcs), true, true)) {
-                return "keyword";
-            }
-            if (pConfig.operators !== null && stream.match(wordRegexp(pConfig.operators), true, true)) {
-                return "operator";
-            }
-            /* attribute lists */
-            if (!pConfig.inAttributeList && stream.match(/\[/, true, true)) {
-                pConfig.inAttributeList = true;
-                return "bracket";
-            }
-            if (pConfig.inAttributeList) {
-                if (pConfig.attributes !== null && stream.match(wordRegexp(pConfig.attributes), true, true)) {
-                    return "attribute";
-                }
-                if (stream.match(/]/, true, true)) {
-                    pConfig.inAttributeList = false;
-                    return "bracket";
-                }
-            }
-            /* strings */
-            if (!state.inString && stream.match(/\"[^\"]*/, true, true)) {
-                state.inString = true;
-                return "string";
-            }
-            if (state.inString) {
-                if (stream.match(/[^\"]*\"/, true, true)) {
-                    state.inString = false;
-                } else {
-                    stream.skipToEnd();
-                }
-                return "string";
-            }
-            // }
-            /* comments */
-            if (!state.inComment) {
-                if (stream.match(/\/\*[^\*\/]*/, true, true)) {
-                    state.inComment = true;
-                    return "comment";
-                }
-                if (stream.match(wordRegexp(pConfig.singlecomment), true, true)) {
-                    stream.skipToEnd();
-                    return "comment";
-                }
-            }
-            if (state.inComment) {
-                if (stream.match(/[^\*\/]*\*\//, true, true)) {
-                    state.inComment = false;
-                } else {
-                    stream.skipToEnd();
-                }
-                return "comment";
-            }
-            var ch = stream.next();
-            return "base";
-        };
-    }
+	function produceCopyStateFunction() {
+		return function(pState) {
+			return {
+				inComment : pState.inComment,
+				inString : pState.inString,
+				inAttributeList : pState.inAttributeList,
+				inScript : pState.inScript
+			};
+		};
+	}
+
+	function produceTokenFunction(pConfig) {
+
+		return function(stream, state) {
+			// if (!pConfig.inScript) {
+			// if (pConfig.keywords !== null && stream.match(wordRegexp(pConfig.keywords), true, true)) {
+			// return "keyword";
+			// }
+			// if (stream.match(/{/, true, true)) {
+			// pConfig.inScript = true;
+			// return "bracket";
+			// }
+			// }
+			// if (pConfig.inScript) {
+			// if (stream.match(/}/, true, true)) {
+			// pConfig.inScript = false;
+			// return "bracket";
+			// }
+			if (stream.match(wordRegexp(pConfig.brackets), true, true)) {
+				return "bracket";
+			}
+			/* comments */
+			if (!state.inComment) {
+				if (stream.match(/\/\*[^\*\/]*/, true, true)) {
+					state.inComment = true;
+					return "comment";
+				}
+				if (stream.match(wordRegexp(pConfig.singlecomment), true, true)) {
+					stream.skipToEnd();
+					return "comment";
+				}
+			}
+			if (state.inComment) {
+				if (stream.match(/[^\*\/]*\*\//, true, true)) {
+					state.inComment = false;
+				} else {
+					stream.skipToEnd();
+				}
+				return "comment";
+			}
+			/* strings */
+			if (!state.inString && stream.match(/\"[^\"]*/, true, true)) {
+				state.inString = true;
+				return "string";
+			}
+			if (state.inString) {
+				if (stream.match(/[^\"]*\"/, true, true)) {
+					state.inString = false;
+				} else {
+					stream.skipToEnd();
+				}
+				return "string";
+			}
+			if (stream.match(wordRegexp(pConfig.options), true, true)) {
+				return "keyword";
+			}
+			if (stream.match(wordRegexpBoundary(pConfig.arcsWords), true, true)) {
+				return "keyword";
+			}
+			if (stream.match(wordRegexp(pConfig.arcsOthers), true, true)) {
+				return "keyword";
+			}
+			if (pConfig.operators !== null && stream.match(wordRegexp(pConfig.operators), true, true)) {
+				return "operator";
+			}
+			/* attribute lists */
+			if (!pConfig.inAttributeList && stream.match(/\[/, true, true)) {
+				pConfig.inAttributeList = true;
+				return "bracket";
+			}
+			if (pConfig.inAttributeList) {
+				if (pConfig.attributes !== null && stream.match(wordRegexp(pConfig.attributes), true, true)) {
+					return "attribute";
+				}
+				if (stream.match(/]/, true, true)) {
+					pConfig.inAttributeList = false;
+					return "bracket";
+				}
+			}
+
+			// }
+
+			var ch = stream.next();
+			return "base";
+		};
+	}
 
 });
 
