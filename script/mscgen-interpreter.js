3e86e9c2c674cb666dbf684e3eb1592d4be96c19
sverweij <sverweij@users.noreply.github.com>
2015-02-15 22:43:21 +0100

    manually dedup comments

--- script/mscgen-interpreter.js
+++ script/mscgen-interpreter.js
@@ -8,47 +8,12 @@
 
  mscgen_js is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with mscgen_js.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- This file is part of mscgen_js.
-
- mscgen_js is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- mscgen_js is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with mscgen_js.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
- This file is part of mscgen_js.
-
- mscgen_js is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- mscgen_js is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNEdq.SS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
  along with mscgen_js.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
