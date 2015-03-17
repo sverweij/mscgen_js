5c44f8b4ca54e3e375ef905d9fe332653c593ca6
sverweij <sverweij@users.noreply.github.com>
2015-03-17 19:03:09 +0100

    put guards over optional executables convert (imagemagik) and optipng

--- utl/png2favico.sh
+++ utl/png2favico.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # adapted from http://www.imagemagick.org/Usage/thumbnails/#favicon
-optipng -quiet $1
+CONVERT=`which convert`
+OPTIPNG=`which optipng`
+if [ $OPTIPNG ]
+then
+    $OPTIPNG -quiet $1
 # convert $1  -bordercolor white -border 0 \
           # \( -clone 0 -resize 16x16 \) \
           # \( -clone 0 -resize 24x24 \) \
@@ -8,11 +12,15 @@ optipng -quiet $1
           # \( -clone 0 -resize 48x48 \) \
           # \( -clone 0 -resize 64x64 \) \
           # -delete 0 -alpha off -colors 256 $2
+fi
 
-convert $1  \
+if [ $CONVERT ]
+then
+    $CONVERT $1  \
           \( -clone 0 -resize 16x16 \) \
           \( -clone 0 -resize 24x24 \) \
           \( -clone 0 -resize 32x32 \) \
           \( -clone 0 -resize 48x48 \) \
           \( -clone 0 -resize 64x64 \) \
           -delete 0 -alpha on $2
+fi
--- utl/resize.sh
+++ utl/resize.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 SIZE=`expr "$2" : "[^-]*-\(.*\).png"`
-convert $1 \( -clone 0 -resize $SIZEx$SIZE \) -delete 0 -alpha on $2
-optipng -quiet $2
+CONVERT=`which convert`
+OPTIPNG=`which optipng`
+if [ $CONVERT ]
+then
+    $CONVERT $1 \( -clone 0 -resize $SIZEx$SIZE \) -delete 0 -alpha on $2
+fi
+
+if [ $OPTIPNG ]
+then
+    $OPTIPNG -quiet $2
+fi
