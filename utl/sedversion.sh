86dd605e5db92f1d6b0a09652f14c559a313a8cf
sverweij <sverweij@users.noreply.github.com>
2015-04-27 21:32:57 +0200

    change replacement algorithm so it works without extended regexes (and hence standard sed)

--- utl/sedversion.sh
+++ utl/sedversion.sh
@@ -5,10 +5,18 @@ DATE=`date "+%Y%m%d %H:%M"`
 HOST=`cat tracking.host`
 TRACKINGID=`cat tracking.id`
 SITEVERIFID=`cat siteverification.id`
-sed  -E s/"([^\$])([\$]{version})"/"\1$VERSION"/g |\
-    sed  -E s/"([^\$])([\$]{commit})"/"\1$COMMIT"/g |\
-    sed  -E s/"([^\$])([\$]{date})"/"\1$DATE"/g |\
-    sed  -E s/"([^\$])([\$]{host})"/"\1$HOST"/g |\
-    sed  -E s/"([^\$])([\$]{trackingid})"/"\1$TRACKINGID"/g |\
-    sed  -E s/"([^\$])([\$]{siteverificationid})"/"\1$SITEVERIFID"/g |\
-    sed s/"\$\${"/"\${"/g
+
+sed s/"{{version}}"/$VERSION/g |\
+    sed s/"{{commit}}"/$COMMIT/g |\
+    sed s/"{{date}}"/"$DATE"/g |\
+    sed s/"{{host}}"/$HOST/g |\
+    sed s/"{{trackingid}}"/$TRACKINGID/g |\
+    sed s/"{{siteverificationid}}"/$SITEVERIFID/g
+
+# sed  -E s/"([^\$])([\$]{version})"/"\1$VERSION"/g |\
+    # sed  -E s/"([^\$])([\$]{commit})"/"\1$COMMIT"/g |\
+    # sed  -E s/"([^\$])([\$]{date})"/"\1$DATE"/g |\
+    # sed  -E s/"([^\$])([\$]{host})"/"\1$HOST"/g |\
+    # sed  -E s/"([^\$])([\$]{trackingid})"/"\1$TRACKINGID"/g |\
+    # sed  -E s/"([^\$])([\$]{siteverificationid})"/"\1$SITEVERIFID"/g |\
+    # sed s/"\$\${"/"\${"/g
