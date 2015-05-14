8d77c141c54ee187adc47bc7c36386d08f80d512
sverweij <sverweij@users.noreply.github.com>
2015-05-14 13:46:28 +0200

    tweak exporter test

--- src/script/test/t_controller-exporter.js
+++ src/script/test/t_controller-exporter.js
@@ -38,8 +38,8 @@ var gAST = {
     ]
   ]
 };
-
 var gSVG = '<svg>just a dummy thing</svg>';
+var gMsc = 'msc{a[label="💩"],b[label="序"],c [label="💩"]; a => b[label="things"], c => b;}';
 
 describe('controller-exporter', function(){
     describe('#toVectorURI', function(){
@@ -53,13 +53,12 @@ describe('controller-exporter', function(){
             } catch(e){
                 return false;
             }
-            
         });
         
     });
     describe('#toHTMLSnippetURI', function(){
         it('should render an URI encoded html file with the passed chart embedded', function(){
-            assert.equal(xport.toHTMLSnippetURI('msc{a[label="💩"],b[label="序"],c [label="💩"]; a => b[label="things"], c => b;}', 'mscgen'), 
+            assert.equal(xport.toHTMLSnippetURI(gMsc, 'mscgen'), 
             "data:text/plain;charset=utf-8,%3C!DOCTYPE%20html%3E%0A%3Chtml%3E%0A%20%20%3Chead%3E%0A%20%20%20%20%3Cmeta%20content%3D'text%2Fhtml%3Bcharset%3Dutf-8'%20http-equiv%3D'Content-Type'%3E%0A%20%20%20%20%3Cscript%20src%3D'https%3A%2F%2Fsverweij.github.io%2Fmscgen_js%2Fmscgen-inpage.js'%20defer%3E%0A%20%20%20%20%3C%2Fscript%3E%0A%20%20%3C%2Fhead%3E%0A%20%20%3Cbody%3E%0A%20%20%20%20%3Cpre%20class%3D'code%20mscgen%20mscgen_js'%20data-language%3D'mscgen'%3E%0Amsc%7Ba%5Blabel%3D%22%F0%9F%92%A9%22%5D%2Cb%5Blabel%3D%22%E5%BA%8F%22%5D%2Cc%20%5Blabel%3D%22%F0%9F%92%A9%22%5D%3B%20a%20%3D%3E%20b%5Blabel%3D%22things%22%5D%2C%20c%20%3D%3E%20b%3B%7D%0A%20%20%20%20%3C%2Fpre%3E%0A%20%20%3C%2Fbody%3E%0A%3C%2Fhtml%3E");
         });
     });
@@ -84,9 +83,7 @@ describe('controller-exporter', function(){
                 search: '?debug="false"&donottrack="true"'
             };
             assert.equal(
-                xport.toDebugURI(lLocation, 
-                                 'msc{a[label="💩"],b[label="序"],c [label="💩"]; a => b[label="things"], c => b;}', 
-                                 'mscgen'), 
+                xport.toDebugURI(lLocation, gMsc, 'mscgen'), 
                                  "data:text/plain;charset=utf-8,http%2F%2Flocalhostmscgen_js%2Findex.html%3Flang%3Dmscgen%26donottrack%3D%22true%22%26debug%3D%22false%22%26msc%3Dmsc%257Ba%255Blabel%253D%2522%25F0%259F%2592%25A9%2522%255D%252Cb%255Blabel%253D%2522%25E5%25BA%258F%2522%255D%252Cc%2520%255Blabel%253D%2522%25F0%259F%2592%25A9%2522%255D%253B%2520a%2520%253D%253E%2520b%255Blabel%253D%2522things%2522%255D%252C%2520c%2520%253D%253E%2520b%253B%257D");
         });
         
@@ -97,9 +94,7 @@ describe('controller-exporter', function(){
                 pathname: "mscgen_js/index.html"
             };
             assert.equal(
-                xport.toDebugURI(lLocation, 
-                                 'msc{a[label="💩"],b[label="序"],c [label="💩"]; a => b[label="things"], c => b;}', 
-                                 'mscgen'), 
+                xport.toDebugURI(lLocation, gMsc, 'mscgen'), 
                                  "data:text/plain;charset=utf-8,http%2F%2Flocalhostmscgen_js%2Findex.html%3Flang%3Dmscgen%26msc%3Dmsc%257Ba%255Blabel%253D%2522%25F0%259F%2592%25A9%2522%255D%252Cb%255Blabel%253D%2522%25E5%25BA%258F%2522%255D%252Cc%2520%255Blabel%253D%2522%25F0%259F%2592%25A9%2522%255D%253B%2520a%2520%253D%253E%2520b%255Blabel%253D%2522things%2522%255D%252C%2520c%2520%253D%253E%2520b%253B%257D");
         });
 
