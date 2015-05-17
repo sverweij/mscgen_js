d5a09fe981a19a7b4c322d6e03fbf10fa78b0ebd
sverweij <sverweij@users.noreply.github.com>
2015-05-17 12:47:44 +0200

    fix graphics rendering unit tests
    - execute tests within jsdom context
    - add border case for empty charts
    - in test code and fixture include source _as is_ instead of after parse &
      stringify
    - add border case for when no source code embedded in resulting svg

--- src/script/test/t_renderast.js
+++ src/script/test/t_renderast.js
@@ -1,43 +1,81 @@
 /* jshint unused: true */
 var renderer = require("../render/graphics/renderast");
 var tst = require ("./testutensils");
-var jsdom = require('jsdom');
+var jsdom = require("jsdom");
 
-
-function ast2svg(pAST, pWindow) {
+function ast2svg(pASTString, pWindow, pIncludeSource, pUseOwnElement) {
     // make a deep copy first, as renderAST actively modifies its input
-    var lFixtureString = JSON.stringify(pAST, null, " ");
-    var lFixture = JSON.parse(lFixtureString);
+    // var lFixtureString = JSON.stringify(pAST, null, " ");
+    var lAST = JSON.parse(pASTString);
     renderer.clean("__svg", pWindow);
-    renderer.renderAST(lFixture, lFixtureString, "__svg", pWindow);
-    return pWindow.document.body.innerHTML;
+    if (pIncludeSource){
+        renderer.renderAST(lAST, pASTString, "__svg", pWindow);
+    } else {
+        renderer.renderAST(lAST, null, "__svg", pWindow);
+    }
+    if (pUseOwnElement){
+        return pWindow.__svg.innerHTML;
+    } else {
+        return pWindow.document.body.innerHTML;
+    }
 }
 
-function processAndCompare(pExpectedFile, pInputFile) {
-    jsdom.env("<html><body></body></html>", function(err, window) {
-        tst.assertequalProcessing(pExpectedFile, pInputFile, function(pInput) {
-            return ast2svg(JSON.parse(pInput), window);
-        });
-    });
-}
+
 
 describe('renderast', function() {
-    describe('#renderAST() - xu everyting', function() {
-        it ('should given given a simple syntax tree, render an svg', function() {
-            processAndCompare('./src/script/test/fixtures/astsimple.svg', //
-            './src/script/test/fixtures/astsimple.json');
-        });
-        it('should render all possible arcs', function() {
-            processAndCompare('./src/script/test/fixtures/test01_all_possible_arcs.svg', //
-            './src/script/test/fixtures/test01_all_possible_arcs.json');
-        });
-        it('should render colors', function() {
-            processAndCompare('./src/script/test/fixtures/rainbow.svg', //
-            './src/script/test/fixtures/rainbow.json');
-        });
-        it('should wrap text in boxes well', function(){
-            processAndCompare('./src/script/test/fixtures/test19_multiline_lipsum.svg',//
-            './src/script/test/fixtures/test19_multiline_lipsum.json');
+    jsdom.env("<html><body></body></html>", function(err, pWindow) {
+        describe('#renderAST in body', function() {
+            function processAndCompare(pExpectedFile, pInputFile, pIncludeSource) {
+                tst.assertequalProcessing(pExpectedFile, pInputFile, function(pInput) {
+                    return ast2svg(pInput, pWindow, pIncludeSource);
+                });
+            }
+            it('should be ok with an empty AST', function(){
+                processAndCompare('./src/script/test/fixtures/astempty.svg', //
+                './src/script/test/fixtures/astempty.json', true);
+            });
+            it ('should given given a simple syntax tree, render an svg', function() {
+                processAndCompare('./src/script/test/fixtures/astsimple.svg', //
+                './src/script/test/fixtures/astsimple.json', true);
+            });
+            it ('should given given a simple syntax tree, render an svg - with source omitted from svg', function() {
+                processAndCompare('./src/script/test/fixtures/astsimplenosource.svg', //
+                './src/script/test/fixtures/astsimple.json', false);
+            });
+            it('should render all possible arcs', function() {
+                processAndCompare('./src/script/test/fixtures/test01_all_possible_arcs.svg', //
+                './src/script/test/fixtures/test01_all_possible_arcs.json', true);
+            });
+            it('should render colors', function() {
+                processAndCompare('./src/script/test/fixtures/rainbow.svg', //
+                './src/script/test/fixtures/rainbow.json', true);
+            });
+            it('should wrap text in boxes well', function(){
+                processAndCompare('./src/script/test/fixtures/test19_multiline_lipsum.svg',//
+                './src/script/test/fixtures/test19_multiline_lipsum.json', true);
+            });
         });
     });
+    jsdom.env("<html><body><span id='__svg'></span></body></html>", function(err, pWindow) {
+        describe('#renderAST in own element', function() {
+            function processAndCompare(pExpectedFile, pInputFile, pIncludeSource, pUseOwnElement) {
+                tst.assertequalProcessing(pExpectedFile, pInputFile, function(pInput) {
+                    return ast2svg(pInput, pWindow, pIncludeSource, pUseOwnElement);
+                });
+            }
+            it('should be ok with an empty AST', function(){
+                processAndCompare('./src/script/test/fixtures/astempty.svg', //
+                './src/script/test/fixtures/astempty.json', true, true);
+            });
+            it ('should given given a simple syntax tree, render an svg', function() {
+                processAndCompare('./src/script/test/fixtures/astsimple.svg', //
+                './src/script/test/fixtures/astsimple.json', true, true);
+            });
+        });
+    });
+    it('dummy so mocha executest the tests wrapped in jsdom', function(){
+        return true;
+    });
+
+
 });
