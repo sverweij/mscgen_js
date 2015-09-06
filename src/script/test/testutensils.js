ef997a4381f8f862a2684edc10b2433bc2f8c560
sverweij <sverweij@users.noreply.github.com>
2015-09-06 13:22:16 +0200

    makes failed testoutput more readable
    more readable then 400k rolling over your terminal anyway
    
    it now uses a hash, which is signifficantly shorter, scroll wise, so it is easier to find which tests failed

--- src/script/test/testutensils.js
+++ src/script/test/testutensils.js
@@ -1,25 +1,35 @@
 var assert = require("assert");
 var fs = require('fs');
+var crypto = require('crypto');
+
+var gHashToUse = [ 'ripemd160', 'md5', 'sha1'].filter(function(h){
+    return crypto.getHashes().indexOf(h) > -1;
+})[0];
 
 module.exports = (function() {
+    
+    function hashit(pString){
+        return crypto.createHash(gHashToUse).update(pString).digest('hex');
+    }
+    
 return {
     assertequalJSON : function(pExpected, pFound) {
-        assert.equal(JSON.stringify(pExpected), JSON.stringify(pFound));
+        assert.equal(hashit(JSON.stringify(pExpected)), hashit(JSON.stringify(pFound)));
     },
 
     assertequalJSONFile : function(pExpectedFileName, pFound) {
         var lJSONFromFile = fs.readFileSync(pExpectedFileName, {"encoding": "utf8"});
-        assert.equal(JSON.stringify(pFound), JSON.stringify(JSON.parse(lJSONFromFile)));
+        assert.equal(hashit(JSON.stringify(pFound)), hashit(JSON.stringify(JSON.parse(lJSONFromFile))));
     },
 
     assertequalToFile : function(pExpectedFileName, pFound) {
-        assert.equal(pFound, fs.readFileSync(pExpectedFileName, {"encoding": "utf8"}));
+        assert.equal(hashit(pFound), hashit(fs.readFileSync(pExpectedFileName, {"encoding": "utf8"})));
     },
     
     assertequalProcessing : function(pExpectedFileName, pInputFileName, pProcessingFn){
         var lExpectedContents = fs.readFileSync(pExpectedFileName, {"encoding" : "utf8"});
         var lInputContents = fs.readFileSync(pInputFileName, {"encoding" : "utf8"});
-        assert.equal(pProcessingFn(lInputContents), lExpectedContents);
+        assert.equal(hashit(pProcessingFn(lInputContents)), hashit(lExpectedContents));
     },
     
     assertSyntaxError: function(pProgram, pParser, pErrorType){
