3ae91a385f7cefec0e84010215885b8b01834c8c
sverweij <sverweij@users.noreply.github.com>
2015-09-19 19:27:10 +0200

    [render] textutensils: move wrap to return statement

--- src/script/render/text/textutensils.js
+++ src/script/render/text/textutensils.js
@@ -18,42 +18,6 @@ define([],
 function() {
     "use strict";
 
-    function _wrap(pText, pMaxLength) {
-        var lCharCount = 0;
-        var lRetval = [];
-        var lStart = 0;
-        var lNewStart = 0;
-        var lEnd = 0;
-
-        var i = 0;
-        var lText = pText.replace(/[\t\n]+/g, " ").replace(/\\n/g, "\n");
-
-        while (i <= lText.length) {
-            if (i >= (lText.length)) {
-                lRetval.push(lText.substring(lStart, i));
-            } else if (lText[i] === '\n') {
-                lCharCount = 0;
-                lEnd = i;
-                lRetval.push(lText.substring(lStart, lEnd));
-                lStart = lEnd + 1;
-            } else if ((lCharCount++ >= pMaxLength)) {
-                lEnd = lText.substring(0, i).lastIndexOf(' ');
-                if (lEnd === -1 || lEnd < lStart) {
-                    lCharCount = 1;
-                    lEnd = i;
-                    lNewStart = i;
-                } else {
-                    lCharCount = 0;
-                    lNewStart = lEnd + 1;
-                }
-                lRetval.push(lText.substring(lStart, lEnd));
-                lStart = lNewStart;
-            }
-            i++;
-        }
-        return lRetval;
-    }
-
     return {
         /**
          * Wraps text on the first space found before pMaxlength,
@@ -61,9 +25,43 @@ function() {
          * Classic "greedy" algorithm.
          * @param {string} pText
          * @param {int} pMaxLength
-         * @return {string}
+         * @return {Array of string}
          */
-        wrap : _wrap,
+        wrap : function (pText, pMaxLength) {
+            var lCharCount = 0;
+            var lRetval = [];
+            var lStart = 0;
+            var lNewStart = 0;
+            var lEnd = 0;
+
+            var i = 0;
+            var lText = pText.replace(/[\t\n]+/g, " ").replace(/\\n/g, "\n");
+
+            while (i <= lText.length) {
+                if (i >= (lText.length)) {
+                    lRetval.push(lText.substring(lStart, i));
+                } else if (lText[i] === '\n') {
+                    lCharCount = 0;
+                    lEnd = i;
+                    lRetval.push(lText.substring(lStart, lEnd));
+                    lStart = lEnd + 1;
+                } else if ((lCharCount++ >= pMaxLength)) {
+                    lEnd = lText.substring(0, i).lastIndexOf(' ');
+                    if (lEnd === -1 || lEnd < lStart) {
+                        lCharCount = 1;
+                        lEnd = i;
+                        lNewStart = i;
+                    } else {
+                        lCharCount = 0;
+                        lNewStart = lEnd + 1;
+                    }
+                    lRetval.push(lText.substring(lStart, lEnd));
+                    lStart = lNewStart;
+                }
+                i++;
+            }
+            return lRetval;
+        },
 
         /**
          * takes pString and replaces all escaped double quotes with
