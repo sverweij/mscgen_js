0925844c38eb7c636df9cf4282c4bd409a5ae92f
sverweij <sverweij@users.noreply.github.com>
2015-12-05 21:50:28 +0100

    [interface] adds usage documentation to index.js

--- src/script/index.js
+++ src/script/index.js
@@ -15,9 +15,29 @@ var gLang2TextRenderer = {
     dot: "./render/text/ast2dot",
     doxygen: "./render/text/ast2doxygen"
 };
-
+/**
+ * Porno
+ * @type {Object}
+ */
 module.exports = {
-
+    /**
+     * Given a sequence chart language, returns a parser that parses
+     * that language into an absract syntax tree
+     *
+     * Supports languages 'mscgen', 'xu' and 'msgenny'. When the
+     * function doesn't recognize the language or when no language is passed
+     * it returns a parser for MscGen
+     *
+     * e.g.
+     * var mscgenjs = require('mscgenjs');
+     *
+     * var parser = mscgenjs.getParser('msgenny');
+     * var msGennyScript = 'alice =>> alice: sign message; alice =>> bob: signed message;';
+     * var abstractSyntaxTree = parser.parse(msGennyScript);
+     *
+     * @param  {string} pLanguage which language to parse.
+     * @return {function}           [description]
+     */
     getParser : function getParser(pLanguage) {
         "use strict";
         return require(
@@ -26,11 +46,38 @@ module.exports = {
               DEFAULT_PARSER
         );
     },
+
+    /**
+     * Returns a function that renders the abstract syntax tree in the given
+     * element and window object.
+     *
+     * e.g.
+     *
+     * var renderer = mscgenjs.getGraphicsRenderer();
+     * var elementId = '__svg';
+     * renderer.clean(elementId, window);
+     * renderer.renderAST(abstractSyntaxTree, msGennyScript, elementId, window);
+     *
+     * @return {function}
+     */
     getGraphicsRenderer : function getGraphicsRenderer(){
         "use strict";
         return require('./render/graphics/renderast');
     },
 
+    /**
+     * Returns a function that, given an abstract syntax tree
+     * renders it in the requested language
+     *
+     * Recognized languages: mscgen, xu, msgenny, dot, doxygen
+     *
+     * e.g.
+     * var textRenderer = mscgenjs.getTextRenderer('mscgen');
+     * var mscGenScript = textRenderer.render(abstractSyntaxTree);
+     *
+     * @param  {string} pLanguage
+     * @return {function}
+     */
     getTextRenderer : function getTextRenderer(pLanguage){
         "use strict";
         return require(
