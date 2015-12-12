7ef7331305ad2aa056bd374e6273deb6fe420d10
sverweij <sverweij@users.noreply.github.com>
2015-12-12 22:29:25 +0100

    [make] updates internal dependencies

--- src/jsdependencies.mk
+++ src/jsdependencies.mk
@@ -2,102 +2,7 @@
 # DO NOT DELETE THIS LINE -- js-makedepend depends on it.
 
 # amd dependencies
-src/script/core/indexAMD.js: \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/parse/msgennyparser.js \
-	src/script/core/parse/xuparser.js \
-	src/script/core/render/graphics/renderast.js \
-	src/script/core/render/text/ast2dot.js \
-	src/script/core/render/text/ast2doxygen.js \
-	src/script/core/render/text/ast2mscgen.js \
-	src/script/core/render/text/ast2msgenny.js \
-	src/script/core/render/text/ast2xu.js
-
-src/script/core/render/graphics/entities.js: \
-	src/script/core/render/graphics/constants.js \
-	src/script/core/render/graphics/renderlabels.js
-
-src/script/core/render/graphics/markermanager.js: \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/render/text/arcmappings.js
-
-src/script/core/render/graphics/renderast.js: \
-	src/script/core/render/graphics/constants.js \
-	src/script/core/render/graphics/entities.js \
-	src/script/core/render/graphics/idmanager.js \
-	src/script/core/render/graphics/markermanager.js \
-	src/script/core/render/graphics/renderlabels.js \
-	src/script/core/render/graphics/renderskeleton.js \
-	src/script/core/render/graphics/renderutensils.js \
-	src/script/core/render/graphics/rowmemory.js \
-	src/script/core/render/graphics/svgelementfactory.js \
-	src/script/core/render/graphics/svgutensils.js \
-	src/script/core/render/graphics/swap.js \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/flatten.js
-
-src/script/core/render/graphics/renderlabels.js: \
-	src/script/core/render/graphics/constants.js \
-	src/script/core/render/graphics/svgelementfactory.js \
-	src/script/core/render/graphics/svgutensils.js \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/textutensils.js
-
-src/script/core/render/graphics/renderskeleton.js: \
-	src/script/core/render/graphics/constants.js \
-	src/script/core/render/graphics/svgelementfactory.js
-
-src/script/core/render/graphics/svgelementfactory.js: \
-	src/script/core/render/graphics/constants.js
-
-src/script/core/render/graphics/svgutensils.js: \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/render/graphics/constants.js \
-	src/script/core/render/graphics/idmanager.js
-
-src/script/core/render/text/ast2animate.js: \
-	src/script/core/lib/lodash/lodash.custom.js
-
-src/script/core/render/text/ast2dot.js: \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/flatten.js \
-	src/script/core/render/text/textutensils.js
-
-src/script/core/render/text/ast2doxygen.js: \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/ast2thing.js \
-	src/script/core/render/text/textutensils.js
-
-src/script/core/render/text/ast2mscgen.js: \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/ast2thing.js \
-	src/script/core/render/text/textutensils.js
-
-src/script/core/render/text/ast2msgenny.js: \
-	src/script/core/render/text/ast2thing.js
-
-src/script/core/render/text/ast2thing.js: \
-	src/script/core/render/text/textutensils.js
-
-src/script/core/render/text/ast2xu.js: \
-	src/script/core/render/text/ast2thing.js \
-	src/script/core/render/text/textutensils.js
-
-src/script/core/render/text/colorize.js: \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/asttransform.js
-
-src/script/core/render/text/flatten.js: \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/asttransform.js \
-	src/script/core/render/text/textutensils.js
-
 src/script/mscgen-inpage.js: \
-	src/script/core/parse/msgennyparser.js \
-	src/script/core/parse/xuparser.js \
-	src/script/core/render/graphics/renderast.js \
 	src/script/ui/embedding/config.js \
 	src/script/ui/embedding/error-rendering.js \
 	src/script/ui/utl/domutl.js \
@@ -112,8 +17,6 @@ src/script/mscgen-interpreter.js: \
 	src/script/ui/interpreter/param-actions.js
 
 src/script/ui/interpreter/animator.js: \
-	src/script/core/render/graphics/renderast.js \
-	src/script/core/render/text/ast2animate.js \
 	src/script/ui/utl/domutl.js \
 	src/script/ui/utl/gaga.js
 
@@ -137,7 +40,6 @@ src/script/ui/interpreter/general-actions.js: \
 	src/script/ui/utl/domutl.js
 
 src/script/ui/interpreter/input-actions.js: \
-	src/script/core/render/text/colorize.js \
 	src/script/ui/interpreter/general-actions.js \
 	src/script/ui/interpreter/uistate.js \
 	src/script/ui/utl/gaga.js \
@@ -156,7 +58,6 @@ src/script/ui/interpreter/output-actions.js: \
 	src/script/ui/interpreter/general-actions.js \
 	src/script/ui/interpreter/raster-exporter.js \
 	src/script/ui/interpreter/uistate.js \
-	src/script/ui/utl/domutl.js \
 	src/script/ui/utl/exporter.js \
 	src/script/ui/utl/gaga.js
 
@@ -173,95 +74,15 @@ src/script/ui/interpreter/raster-exporter.js: \
 	src/lib/canvg/rgbcolor.js
 
 src/script/ui/interpreter/uistate.js: \
-	src/script/core/parse/msgennyparser.js \
-	src/script/core/parse/xuparser.js \
-	src/script/core/render/graphics/renderast.js \
-	src/script/core/render/text/ast2msgenny.js \
-	src/script/core/render/text/ast2xu.js \
 	src/script/ui/utl/domutl.js \
 	src/script/ui/utl/exporter.js \
 	src/script/ui/utl/gaga.js \
 	src/script/ui/utl/maps.js
 
 src/script/ui/utl/exporter.js: \
-	src/script/core/render/text/ast2dot.js \
-	src/script/core/render/text/ast2doxygen.js \
-	src/script/core/render/text/ast2mscgen.js \
 	src/script/ui/utl/paramslikker.js
 
 # cjs dependencies
-src/script/core/index.js: \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/render/graphics/renderast.js
-
-src/script/core/test/parse/t_mscgenparser_node.js: \
-	src/script/core/parse/mscgenparser_node.js \
-	src/script/core/test/astfixtures.js \
-	src/script/core/test/testutensils.js
-
-src/script/core/test/parse/t_msgennyparser_node.js: \
-	src/script/core/parse/msgennyparser_node.js \
-	src/script/core/test/astfixtures.js \
-	src/script/core/test/testutensils.js
-
-src/script/core/test/parse/t_xuparser_node.js: \
-	src/script/core/parse/xuparser_node.js \
-	src/script/core/test/astfixtures.js \
-	src/script/core/test/testutensils.js
-
-src/script/core/test/render/graphics/t_markermanager.js: \
-	src/script/core/render/graphics/markermanager.js
-
-src/script/core/test/render/graphics/t_renderast.js: \
-	src/script/core/render/graphics/renderast.js \
-	src/script/core/test/testutensils.js
-
-src/script/core/test/render/text/t_ast2animate.js: \
-	src/script/core/parse/xuparser_node.js \
-	src/script/core/render/text/ast2animate.js \
-	src/script/core/test/astfixtures.js \
-	src/script/core/test/testutensils.js
-
-src/script/core/test/render/text/t_ast2dot.js: \
-	src/script/core/render/text/ast2dot.js \
-	src/script/core/test/astfixtures.js
-
-src/script/core/test/render/text/t_ast2doxygen.js: \
-	src/script/core/render/text/ast2doxygen.js \
-	src/script/core/test/astfixtures.js
-
-src/script/core/test/render/text/t_ast2mscgen.js: \
-	src/script/core/parse/mscgenparser_node.js \
-	src/script/core/render/text/ast2mscgen.js \
-	src/script/core/test/astfixtures.js
-
-src/script/core/test/render/text/t_ast2msgenny.js: \
-	src/script/core/render/text/ast2msgenny.js \
-	src/script/core/test/astfixtures.js \
-	src/script/core/test/testutensils.js
-
-src/script/core/test/render/text/t_ast2xu.js: \
-	src/script/core/parse/xuparser_node.js \
-	src/script/core/render/text/ast2xu.js \
-	src/script/core/test/astfixtures.js
-
-src/script/core/test/render/text/t_colorize.js: \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/render/text/colorize.js \
-	src/script/core/test/astfixtures.js
-
-src/script/core/test/render/text/t_flatten.js: \
-	src/script/core/render/text/flatten.js \
-	src/script/core/test/astfixtures.js
-
-src/script/core/test/render/text/t_textutensils.js: \
-	src/script/core/render/text/textutensils.js
-
-src/script/core/test/t_index.js: \
-	src/script/core/index.js \
-	src/script/core/test/astfixtures.js \
-	src/script/core/test/testutensils.js
-
 src/script/ui/test/embedding/t_config.js: \
 	src/script/ui/embedding/config.js
 
@@ -282,29 +103,6 @@ src/script/ui/test/utl/t_store.js: \
 
 # amd dependencies
 EMBED_JS_SOURCES=src/script/mscgen-inpage.js \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/parse/msgennyparser.js \
-	src/script/core/parse/xuparser.js \
-	src/script/core/render/graphics/constants.js \
-	src/script/core/render/graphics/entities.js \
-	src/script/core/render/graphics/idmanager.js \
-	src/script/core/render/graphics/markermanager.js \
-	src/script/core/render/graphics/renderast.js \
-	src/script/core/render/graphics/renderlabels.js \
-	src/script/core/render/graphics/renderskeleton.js \
-	src/script/core/render/graphics/renderutensils.js \
-	src/script/core/render/graphics/rowmemory.js \
-	src/script/core/render/graphics/svgelementfactory.js \
-	src/script/core/render/graphics/svgutensils.js \
-	src/script/core/render/graphics/swap.js \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/ast2dot.js \
-	src/script/core/render/text/ast2doxygen.js \
-	src/script/core/render/text/ast2mscgen.js \
-	src/script/core/render/text/ast2thing.js \
-	src/script/core/render/text/asttransform.js \
-	src/script/core/render/text/flatten.js \
-	src/script/core/render/text/textutensils.js \
 	src/script/ui/embedding/config.js \
 	src/script/ui/embedding/error-rendering.js \
 	src/script/ui/utl/domutl.js \
@@ -325,33 +123,6 @@ INTERPRETER_JS_SOURCES=src/script/mscgen-interpreter.js \
 	src/lib/codemirror/lib/codemirror.js \
 	src/lib/codemirror/mode/javascript/javascript.js \
 	src/lib/codemirror/mode/mscgen/mscgen.js \
-	src/script/core/lib/lodash/lodash.custom.js \
-	src/script/core/parse/msgennyparser.js \
-	src/script/core/parse/xuparser.js \
-	src/script/core/render/graphics/constants.js \
-	src/script/core/render/graphics/entities.js \
-	src/script/core/render/graphics/idmanager.js \
-	src/script/core/render/graphics/markermanager.js \
-	src/script/core/render/graphics/renderast.js \
-	src/script/core/render/graphics/renderlabels.js \
-	src/script/core/render/graphics/renderskeleton.js \
-	src/script/core/render/graphics/renderutensils.js \
-	src/script/core/render/graphics/rowmemory.js \
-	src/script/core/render/graphics/svgelementfactory.js \
-	src/script/core/render/graphics/svgutensils.js \
-	src/script/core/render/graphics/swap.js \
-	src/script/core/render/text/arcmappings.js \
-	src/script/core/render/text/ast2animate.js \
-	src/script/core/render/text/ast2dot.js \
-	src/script/core/render/text/ast2doxygen.js \
-	src/script/core/render/text/ast2mscgen.js \
-	src/script/core/render/text/ast2msgenny.js \
-	src/script/core/render/text/ast2thing.js \
-	src/script/core/render/text/ast2xu.js \
-	src/script/core/render/text/asttransform.js \
-	src/script/core/render/text/colorize.js \
-	src/script/core/render/text/flatten.js \
-	src/script/core/render/text/textutensils.js \
 	src/script/ui/interpreter/animator.js \
 	src/script/ui/interpreter/editor-events.js \
 	src/script/ui/interpreter/general-actions.js \
