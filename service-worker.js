123b87a42c9e063944805c38d4e7aeb2c4cf1a40
sverweij <sverweij@users.noreply.github.com>
2021-01-01 20:00:05 +0100

    build 3.1.19
    webp

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["favicon.ico","e506e1a72dd8ae3a51879bd833c891f2"],["fonts/controls.eot","b4f8c56df781129de8b2d674d8cc2266"],["fonts/controls.svg","d0ef06d868d3e5c00dfc475aaac8a182"],["fonts/controls.ttf","ac78cd67fd6e829a892b8766618ba6b0"],["fonts/controls.woff","4a7eb48551c22b9a375bfef930376c29"],["images/bg.png","c4b0a5e4481fcf135ab2644dea10433f"],["images/mugshot.jpg","21595a6357a652c501c3606b946850a5"],["images/test14_cheat_sheet.svg","7cffdc89e810917ed25a828494b4972c"],["index.html","23344fda835a170f84a3a67dfe05cce9"],["manifest.json","7e714594d5fe602d005ba778201e455f"],["mscgen-interpreter.min.js","804a8968fd962455f475b2d149b529d4"],["style/interp.css","349e6116c09c5b3e50d126b8e51af713"]];
+var precacheConfig = [["favicon.ico","e506e1a72dd8ae3a51879bd833c891f2"],["fonts/controls.eot","b4f8c56df781129de8b2d674d8cc2266"],["fonts/controls.svg","d0ef06d868d3e5c00dfc475aaac8a182"],["fonts/controls.ttf","ac78cd67fd6e829a892b8766618ba6b0"],["fonts/controls.woff","4a7eb48551c22b9a375bfef930376c29"],["images/bg.png","c4b0a5e4481fcf135ab2644dea10433f"],["images/mugshot.webp","4cebef132e859e7f09d0c9cdcf273ed1"],["images/test14_cheat_sheet.svg","7cffdc89e810917ed25a828494b4972c"],["index.html","c8193eb3ff76d1e9a940e1c8c3322961"],["manifest.json","7e714594d5fe602d005ba778201e455f"],["mscgen-interpreter.min.js","b5e67688e948f4850473dd4fb4a3f402"],["style/interp.css","179054754d2b861fc65123f9b5d94497"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
