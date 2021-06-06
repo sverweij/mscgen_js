89a2c49e842a2cbe30b846f319b23f2ea003c481
sverweij <sverweij@users.noreply.github.com>
2021-06-06 14:05:35 +0200

    build 4.0.0 (update the inpage script to the 'lazy loaded' variant)

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["favicon.ico","e506e1a72dd8ae3a51879bd833c891f2"],["fonts/controls.eot","b4f8c56df781129de8b2d674d8cc2266"],["fonts/controls.svg","d0ef06d868d3e5c00dfc475aaac8a182"],["fonts/controls.ttf","ac78cd67fd6e829a892b8766618ba6b0"],["fonts/controls.woff","4a7eb48551c22b9a375bfef930376c29"],["images/bg.png","c4b0a5e4481fcf135ab2644dea10433f"],["images/mugshot.webp","4cebef132e859e7f09d0c9cdcf273ed1"],["images/test14_cheat_sheet.svg","7cffdc89e810917ed25a828494b4972c"],["index.html","74bad5a7ede1a31981e0bd5e0b605595"],["manifest.json","7e714594d5fe602d005ba778201e455f"],["mscgen-interpreter.min.js","59e5be7fe5756bf792f1dd192ede0d46"],["style/interp.css","bdc5d4c1b0d0df5a38944fc6d0875269"]];
+var precacheConfig = [["favicon.ico","e506e1a72dd8ae3a51879bd833c891f2"],["fonts/controls.eot","b4f8c56df781129de8b2d674d8cc2266"],["fonts/controls.svg","d0ef06d868d3e5c00dfc475aaac8a182"],["fonts/controls.ttf","ac78cd67fd6e829a892b8766618ba6b0"],["fonts/controls.woff","4a7eb48551c22b9a375bfef930376c29"],["images/bg.png","c4b0a5e4481fcf135ab2644dea10433f"],["images/mugshot.webp","4cebef132e859e7f09d0c9cdcf273ed1"],["images/test14_cheat_sheet.svg","7cffdc89e810917ed25a828494b4972c"],["index.html","42370f9fd6cafddca5073f36628247b8"],["manifest.json","7e714594d5fe602d005ba778201e455f"],["mscgen-interpreter.min.js","59e5be7fe5756bf792f1dd192ede0d46"],["style/interp.css","bdc5d4c1b0d0df5a38944fc6d0875269"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
